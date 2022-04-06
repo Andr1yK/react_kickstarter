21f9370f6ceed4cddbe9a829de462644d61a5a0b
Andriy Kobetiak <andriykobetiak@gmail.com>
2022-04-06 13:21:40 +0300

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,6 @@
-// Uncomment eslint-config-react if you want use react without TS
-
 module.exports = {
   extends: [
     '@mate-academy/eslint-config-react-typescript',
     'plugin:cypress/recommended',
   ],
-  // extends: ['@mate-academy/eslint-config-react', 'plugin:cypress/recommended'],
-  rules: {
-    'object-curly-newline': [
-      'off',
-    ],
-  },
 };
