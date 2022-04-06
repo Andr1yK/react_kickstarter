e59dd495982eee44a9428e832d1b24a455abd69b
Andriy Kobetiak <andriykobetiak@gmail.com>
2022-04-06 11:00:52 +0300

    Implement custom hook (useSelector)

--- src/hooks/useSelector.ts
+++ src/hooks/useSelector.ts
@@ -2,6 +2,6 @@ import { useContext } from 'react';
 import { StateContext } from '../store';
 import { Store } from '../types';
 
-export const useSelector = (callback: (store: Store) => any) => {
+export const useSelector = <T>(callback: (store: Store) => ReturnType<(store: Store) => T>) => {
   return callback(useContext(StateContext));
 };
