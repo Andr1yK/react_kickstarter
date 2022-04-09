d4c2376c0544e6c83fdb747fee0f3949447060bb
Andriy Kobetiak <andriykobetiak@gmail.com>
2022-04-09 16:18:59 +0300

    Update configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,16 @@ module.exports = {
     'plugin:cypress/recommended',
     "plugin:react-hooks/recommended",
   ],
-  "rules": {
+  rules: {
+    'max-len': ['error', {
+      ignoreTemplateLiterals: true,
+      ignoreComments: true,
+      code: 100,
+      ignoreUrls: true
+    }],
     "no-console": "warn",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "error",
-    "react/require-default-props": "off"
-  }
+    "react/require-default-props": "off",
+  },
 };
