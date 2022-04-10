18662e9f3d6c1c7d837467debfba9d35a501ac9a
Andriy Kobetiak <andriykobetiak@gmail.com>
2022-04-10 12:34:04 +0300

    Fix App imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { FC, memo } from 'react';
 import './App.scss';
-import Home from './components/Home/Home';
+import Home from './components';
 
 import './layouts/Grid/Grid.scss'; // костиль, переписати апку на Grid i GridItem
 
