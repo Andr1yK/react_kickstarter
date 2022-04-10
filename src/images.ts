6bbf5d83056db528f30f4cd707090bce9c68c425
Andriy Kobetiak <andriykobetiak@gmail.com>
2022-04-10 13:19:33 +0300

    Move logo.svg to same component

--- src/images.ts
+++ src/images.ts
@@ -1,9 +1,7 @@
-import logo from './assets/images/logo.svg';
 import headerImage from './assets/images/header-1024w.png';
 import featuresImage from './assets/images/features-1920w.png';
 
 export {
-  logo,
   headerImage,
   featuresImage,
 };
