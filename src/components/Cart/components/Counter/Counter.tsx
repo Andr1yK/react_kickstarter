50af7b9c843a9a5489ce2b2590bb09b30e8e6642
Andriy Kobetiak <andriyk@MacBook-Pro-Andriy.local>
2022-04-17 13:10:40 +0300

    Add interactivity to Counter

--- src/components/Cart/components/Counter/Counter.tsx
+++ src/components/Cart/components/Counter/Counter.tsx
@@ -1,16 +1,49 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable react/button-has-type */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import classNames from 'classnames';
-import { memo, FC } from 'react';
+import {
+  memo,
+  FC,
+  useState,
+  ChangeEvent,
+  useCallback,
+} from 'react';
 
 import style from './Counter.module.scss';
 
 type Props = {
   className?: string,
+  initialValue?: number,
 };
 
-export const Counter: FC<Props> = memo(({ className = '' }) => {
+export const Counter: FC<Props> = memo(({ className = '', initialValue = 0 }) => {
+  const [count, setCount] = useState(initialValue);
+
+  const getValidCount = useCallback((value: number, currentValue: number) => {
+    if (value < 0) {
+      return 0;
+    }
+
+    if (value > 99) {
+      return 99;
+    }
+
+    if (Number.isNaN(value)) {
+      return currentValue;
+    }
+
+    return value;
+  }, []);
+
+  const handleChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    const value = Number(event.target.value);
+
+    setCount((currentCount) => getValidCount(value, currentCount));
+  }, [getValidCount]);
+
+  const handleClick = useCallback((step: number) => () => {
+    setCount((currentCount) => getValidCount(currentCount + step, currentCount));
+  }, [getValidCount]);
+
   return (
     <div
       className={classNames(
@@ -19,24 +52,30 @@ export const Counter: FC<Props> = memo(({ className = '' }) => {
       )}
     >
       <button
-        disabled
+        type="button"
+        disabled={count <= 0}
+        onClick={handleClick(-1)}
         className={classNames(
           style.counter__button,
           style['counter__button--minus'],
         )}
       />
-      <label>
+      <label htmlFor="count">
         <input
-          type="number"
+          type="text"
           name="count"
           className={classNames(
             style['counter__count-input'],
             style['counter__count-input--large'],
           )}
-          value="1"
+          value={count}
+          onChange={handleChange}
         />
       </label>
       <button
+        type="button"
+        disabled={count >= 99}
+        onClick={handleClick(1)}
         className={classNames(
           style.counter__button,
           style['counter__button--plus'],
