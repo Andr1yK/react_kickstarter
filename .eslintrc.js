3a542f1d8ba90d6afc96d4c3058fe2028a7c9d32
Andriy Kobetiak <andriykobetiak@gmail.com>
2022-06-12 14:01:21 +0300

    Configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
   rules: {
-    'react/require-default-props': 'off',
+    'react/require-default-props': ['error', {
+      classes: 'defaultProps',
+      functions: 'defaultArguments',
+    }],
     'max-len': ['error', {
       ignoreTemplateLiterals: true,
       ignoreStrings: true,
