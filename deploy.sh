608e73dac80294032c163f37447d3b999b01c750
andr1yk <andriykobetiak@bots.github.com>
2022-06-12 17:23:55 +0300

    Deploy solution

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,6 @@ git config user.name "andr1yk"
 git config user.email "andriykobetiak@bots.github.com"
 
 git add dist && git commit -m "Update gh-page"
-git push origin `git subtree split --prefix dist main`:gh-pages --force
+git checkout -b gh-pages-temp
+git push -f origin `git subtree split --prefix dist -b gh-pages`:gh-pages
+git branch -D gh-pages
