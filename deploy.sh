f44b1347f95464dacab96b2bdcc2d35c5d1c8e0a
andr1yk <andriykobetiak@bots.github.com>
2022-06-12 17:11:23 +0300

    Update .gitignore

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ git config user.name "andr1yk"
 git config user.email "andriykobetiak@bots.github.com"
 
 git add dist && git commit -m "Update gh-page"
-git subtree push --prefix dist origin gh-pages
+git push origin `git subtree split --prefix dist gh-pages` --force
