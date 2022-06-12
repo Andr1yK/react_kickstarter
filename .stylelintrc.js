394c2bf7d457e27fd726ac82b1b7539b73343f0f
Andriy Kobetiak <andriykobetiak@gmail.com>
2022-06-12 15:30:12 +0300

    Fix

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "plugins": [
+    "stylelint-scss"
+  ],
+  "extends": "stylelint-config-recommended-scss",
+  "rules": {
+    "custom-property-empty-line-before": "never"
+  }
+};
