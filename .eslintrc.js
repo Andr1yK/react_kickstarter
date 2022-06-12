02e360af51b6a14480812203a27a22c7246a895c
Andriy Kobetiak <andriykobetiak@gmail.com>
2022-06-12 15:39:34 +0300

    Fix 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,22 @@
 module.exports = {
+  parserOptions: {
+    project: 'tsconfig.eslint.json',
+    sourceType: 'module',
+  },
   extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
   rules: {
+    'react/require-default-props': ['error', {
+      classes: 'defaultProps',
+      functions: 'defaultArguments',
+    }],
     'max-len': ['error', {
       ignoreTemplateLiterals: true,
+      ignoreStrings: true,
       ignoreComments: true,
+      ignoreRegExpLiterals: true,
     }],
-    'jsx-a11y/label-has-associated-control': ["error", {
-      assert: "either",
+    'jsx-a11y/label-has-associated-control': ['error', {
+      assert: 'either',
     }],
   },
 };
