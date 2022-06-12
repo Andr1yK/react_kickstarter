190c87b1292e38197ed6172ab86c33df4bb1328a
andr1yk <andriykobetiak@bots.github.com>
2022-06-12 17:06:56 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,6 @@
 
 git config user.name "andr1yk"
 git config user.email "andriykobetiak@bots.github.com"
+
+git add dist && git commit -m "Update gh-page"
+git subtree push --prefix dist origin gh-pages
